f66d456b31b7f4baa42b2e6b82fdf1e6501692c2
StevenMad <stevenmadec@live.fr>
2022-08-12 02:45:16 +0200

    1st commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PriceCard from "../components/cards/PriceCard";
+
+const Home = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row col-md-6 offset-md-3 text-center">
+        <h1 className="pt-5 fw-bold">
+          Explore the right plan for your business
+        </h1>
+        <p className="lead pb-4">Choose a plan that suites you best!</p>
+      </div>
+
+      <div className="row pt-5 mb-3 text-center">
+        <PriceCard />
+        <PriceCard />
+        <PriceCard />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
